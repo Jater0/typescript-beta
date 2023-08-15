8f86526e02f6db48043f8d048165db0f4aac63f9
Jater Chu <1452606865@qq.com>
2023-08-16 00:09:05 +0800

    typescript dev env setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typescript 5.2 Previews
+
+## How to run it
+``` sh
+npm install
+npx tsc
+```
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import "core-js/modules/esnext.symbol.dispose.js";
+import "core-js/modules/esnext.symbol.async-dispose.js";
+import "core-js/modules/esnext.disposable-stack.constructor.js";
